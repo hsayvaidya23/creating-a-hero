c0415a3d902626bc9d4cb313914d2749f8fb6154
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-04-23 12:33:18 +0530

    Condictional Rendering

--- src/App.js
+++ src/App.js
@@ -6,47 +6,63 @@ function App() {
   const [age, setAge] = useState(0);
   const [height, setHeight] = useState(0);
   const [superPower, setSuperPower] = useState("");
+  const [displayCharacter, setDisplayCharacter] = useState(false);
 
   return (
     <div className="App">
       <h1>Build A Hero</h1>
-      <label htmlFor="">Name: </label>
-      <input
-        type="text"
-        style={{ margin: 10 }}
-        onChange={(event) => {
-          setName(event.target.value);
+      <div>
+        <label htmlFor="">Name: </label>
+        <input
+          type="text"
+          style={{ margin: 10 }}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label htmlFor="">Age: </label>
+        <input
+          type="number"
+          style={{ margin: 10 }}
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <label htmlFor="">Height: </label>
+        <input
+          type="number"
+          style={{ margin: 10 }}
+          onChange={(event) => {
+            setHeight(event.target.value);
+          }}
+        />
+        <label htmlFor="">SuperPower: </label>
+        <input
+          type="text"
+          style={{ margin: 10 }}
+          onChange={(event) => {
+            setSuperPower(event.target.value);
+          }}
+        />
+      </div>
+      <button
+        onClick={() => {
+          setDisplayCharacter(true);
         }}
-      />
-      <label htmlFor="">Age: </label>
-      <input
-        type="number"
-        style={{ margin: 10 }}
-        onChange={(event) => {
-          setAge(event.target.value);
-        }}
-      />
-      <label htmlFor="">Height: </label>
-      <input
-        type="number"
-        style={{ margin: 10 }}
-        onChange={(event) => {
-          setHeight(event.target.value);
-        }}
-      />
-      <label htmlFor="">SuperPower: </label>
-      <input
-        type="text"
-        style={{ margin: 10 }}
-        onChange={(event) => {
-          setSuperPower(event.target.value);
-        }}
-      />
-
-      {name}
-      {age}
-      {height}
-      {superPower}
+      >
+        Display Character
+      </button>
+      <div>
+        <h1>Hero Info</h1>
+        {displayCharacter && (
+          <ul>
+            <li>Name: {name}</li>
+            <li>Age: {age}</li>
+            <li>Height: {height}</li>
+            <li>SuperPower: {superPower}</li>
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
