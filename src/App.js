a9c787dfea19df04da6e9f1de0c5bc7f0f82bedf
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-04-23 12:07:06 +0530

    Changing States

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [name, setName] = useState("JOHN");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [superPower, setSuperPower] = useState("");
 
-  const changeName = () => {
-    setName("Jake");
-  }
   return (
     <div className="App">
       <h1>Build A Hero</h1>
       <label htmlFor="">Name: </label>
-      <input type="text" style={{ margin: 10 }} />
+      <input
+        type="text"
+        style={{ margin: 10 }}
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
       <label htmlFor="">Age: </label>
-      <input type="number" style={{ margin: 10 }} />
+      <input
+        type="number"
+        style={{ margin: 10 }}
+        onChange={(event) => {
+          setAge(event.target.value);
+        }}
+      />
       <label htmlFor="">Height: </label>
-      <input type="text" style={{ margin: 10 }} />
+      <input
+        type="number"
+        style={{ margin: 10 }}
+        onChange={(event) => {
+          setHeight(event.target.value);
+        }}
+      />
       <label htmlFor="">SuperPower: </label>
-      <input type="text" style={{ margin: 10 }} />
+      <input
+        type="text"
+        style={{ margin: 10 }}
+        onChange={(event) => {
+          setSuperPower(event.target.value);
+        }}
+      />
 
-      <button onClick={changeName} >Change Name</button>
       {name}
+      {age}
+      {height}
+      {superPower}
     </div>
   );
 }
