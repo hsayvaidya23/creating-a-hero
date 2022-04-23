7b4f0a433bbc1371c66bc001debc42b321733d51
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-04-23 11:52:31 +0530

    Using States

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("JOHN");
+
+  const changeName = () => {
+    setName("Jake");
+  }
   return (
     <div className="App">
       <h1>Build A Hero</h1>
       <label htmlFor="">Name: </label>
-      <input type="text" />
+      <input type="text" style={{ margin: 10 }} />
       <label htmlFor="">Age: </label>
-      <input type="number" />
+      <input type="number" style={{ margin: 10 }} />
       <label htmlFor="">Height: </label>
-      <input type="text" />
+      <input type="text" style={{ margin: 10 }} />
       <label htmlFor="">SuperPower: </label>
-      <input type="text" />
+      <input type="text" style={{ margin: 10 }} />
+
+      <button onClick={changeName} >Change Name</button>
+      {name}
     </div>
   );
 }
